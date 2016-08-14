0afee8675463cfbf08273c56039f7421400287ca
marcbenav <m.benav86@gmail.com>
2016-08-13 20:32:17 -0400

    Output for number of items on list

--- exercise6.rb
+++ exercise6.rb
@@ -3,5 +3,6 @@ grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 def list(items)
   items.each {|x| puts "*#{x}"}
 end
-
 list(grocery_list)
+
+puts "The list now includes #{grocery_list.length} items"
