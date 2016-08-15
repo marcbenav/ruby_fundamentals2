4677d72f935ac7e0cfd642370a0ddc07a73ca894
marcbenav <m.benav86@gmail.com>
2016-08-15 00:01:38 -0400

    Created and called method to output growth in students

--- exercise7.rb
+++ exercise7.rb
@@ -6,9 +6,20 @@ students = {
 def attendance(info)
   info.each {|k, v| puts "#{k}: #{v} students"}
 end
+
 # method to display hash info
 attendance(students)
+puts "***********************"
 #Added cohort4 to the hash
-students[:cohort4] = "43"
+
+students[:cohort4] = 43
+attendance(students)
+puts "***********************"
 # use keys method to output key names
 puts students.keys
+puts "***********************"
+# Show 5% growth
+def expand(growth)
+growth.each {|k, v| puts "#{k}: #{v * 1.05}"}
+end
+expand(students)
